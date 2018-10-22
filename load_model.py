d6fbe37419199d440e0cc4124d9653cbbca728f5
haiqi0789 <haiqi0789@163.com>
2018-10-22 20:17:37 +0800

    new code

--- load_model.py
+++ load_model.py
@@ -3,13 +3,45 @@ from pandas import Series,DataFrame
 
 
 def min_max_func(headers, db):
-    from sklearn.preprocessing import MinMaxScaler
-    min_max_scaler = MinMaxScaler(feature_range=(-1, 1))
+    #from sklearn.preprocessing import MinMaxScaler
+    #min_max_scaler = MinMaxScaler(feature_range=(-1, 1))
+
+    #consistent_gets
+    db[headers[0]] = (float(db[headers[0]].values) - 47343)/(2047706 - 47343)
+    #db[headers[1]] = (float(db[headers[1]].values) - 1) / (1 - 1)
+    #execute_count
+    db[headers[2]] = (float(db[headers[2]].values) - 758)/(127039 - 758)
+    #parse_count_hard
+    db[headers[3]] = (float(db[headers[3]].values) - 28)/(935 - 28)
+    #parse_count_total
+    db[headers[4]] = (float(db[headers[4]].values) - 92)/(44104 - 92)
+    #session_logical_reads
+    db[headers[5]] = (float(db[headers[5]].values) - 47754) / (2124772 - 47754)
+    #total_sessions
+    db[headers[6]] = (float(db[headers[6]].values) - 6539) / (7017 - 6539)
+    #user_calls
+    db[headers[7]] = (float(db[headers[7]].values) - 876) / (155700 - 876)
+    #user_cpu
+    db[headers[8]] = (float(db[headers[8]].values) - 281.166667) / (7161.333333 - 281.166667)
+
+    '''
     for i in range(0, len(headers)):
+        print(db[headers[i]])
         db[headers[i]] = min_max_scaler.fit_transform(db[headers[i]].values.reshape(-1, 1))
+        print(db[headers[i]])
+    '''
     return db
 
-
+'''
+cpu max = 7161.333333      min = 281.166667
+total_sessions max = 7017       min = 6539
+session_logical_reads max = 2124772     min = 47754
+consistent_gets max = 2047706   min = 47343
+parse_count_total max = 44104   min = 92
+parse_count_hard max = 935      min = 28
+execute_count  max = 127039     min = 758
+user_call max = 155700  min = 876
+'''
 def prepare_data(data):
     objective = data['USER_CPU']
     # drop objective and time
@@ -30,7 +62,7 @@ def svm_test(test_data):
     # print(max, min)
     # scale all data
     test_data = min_max_func(test_data.columns, test_data)
-
+    print(test_data)
 
     #do stuff
 
@@ -55,12 +87,14 @@ def svm_test(test_data):
     y_pred = clf.predict(X_test)
     #print(y_pred)
     # restore predict
-    prediction = ((y_pred + 1)/2) * (7324 - 315) + 315
+    prediction = ((y_pred + 1)/2) * (7161.333333 - 281.166667) + 281.166667
     print(y_pred)
     print(prediction)
 
     features_importance = clf.coef_[0]
     print(features_importance)
+    features_intercept = clf.intercept_
+    print(features_intercept)
     return features_importance, prediction
     #draw_features_importance(clf)
 
--- test.py
+++ test.py
@@ -2,8 +2,21 @@ from pandas import Series,DataFrame
 from load_model import svm_test
 
 
-data = {'DB_ID': ['1'], 'EXECUTE_COUNT': ['1904'], 'SESSION_LOGICAL_READS': ['93691'],
-        'USER_CPU': ['468.3333333'], 'TOTAL_SESSIONS': ['6635'], 'USER_CALLS': ['22022'], 'CONSISTENT_GETS': ['92742'],
-        'PARSE_COUNT_TOTAL': ['525'], 'PARSE_COUNT_HARD': ['46']}
+#data = {'DB_ID': ['1'], 'EXECUTE_COUNT': ['1904'], 'SESSION_LOGICAL_READS': ['93691'],
+#       'USER_CPU': ['468.3333333'], 'TOTAL_SESSIONS': ['6635'], 'USER_CALLS': ['22022'], 'CONSISTENT_GETS': ['92742'],
+#       'PARSE_COUNT_TOTAL': ['525'], 'PARSE_COUNT_HARD': ['46']}
+data = {'DB_ID': ['1'], 'EXECUTE_COUNT': ['69568'], 'SESSION_LOGICAL_READS': ['1476653'],
+       'USER_CPU': ['5309.666667'], 'TOTAL_SESSIONS': ['6782'], 'USER_CALLS': ['112764'], 'CONSISTENT_GETS': ['1402533'],
+       'PARSE_COUNT_TOTAL': ['32256'], 'PARSE_COUNT_HARD': ['636']}
 print(type(data))
 svm_test(data)
+'''
+cpu max = 7161.333333      min = 281.166667
+total_sessions max = 7017       min = 6539
+session_logical_reads max = 2124772     min = 47754
+consistent_gets max = 2047706   min = 47343
+parse_count_total max = 44104   min = 92
+parse_count_hard max = 935      min = 28
+execute_count  max = 127039     min = 758
+user_call max = 155700  min = 876
+'''
